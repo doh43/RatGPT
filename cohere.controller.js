366dbcee641a5fc8f3a5eda8a8b68ee614f1ca53
doh43 <ohdaniel43@gmail.com>
2023-02-14 13:59:13 -0500

    Add test event

--- cohere.controller.js
+++ cohere.controller.js
@@ -1,4 +1,33 @@
 const cohere = require('cohere-ai');
 cohere.init('pJ3B7aXgnujfsLGZ6Tc1b6kGxZQ4Nj6EzoZFZ4wj');
 
-const coherePromptTemplate = "";
+
+const asyncGetCohereGeneration = async function(coherePrompt) {
+	const response = await cohere.generate({
+		model: 'command-medium-nightly',
+		prompt: coherePrompt,
+		max_tokens: 200,
+		temperature: 0.7,
+		num_generations: 5,
+		k: 0,
+		p: 1,
+		frequency_penalty: 0.5,
+		presence_penalty: 0,
+		stop_sequences: ['--'],
+		return_likelihoods: 'GENERATION',
+	});
+	if (response.statusCode !== 200) {
+		if (response.statusCode === 403) {
+			response.status(403).send(response.body);
+		}
+		else {
+			throw new Error(response.body.error.message);
+		}
+	}
+	if (response.body.generations) {
+		return response.body.generations;
+	}
+	else {
+		throw new Error('Unsuccessful Request');
+	}
+};
--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
+const env = require('dotenv').config(); // what
 const { token } = require('./config.json');
+const cohere = require('cohere-ai');
+// const axios = require("axios");
+
+const COHERE_API_KEY = process.env.COHERE_API_KEY
+const COHERE_FEEDBACK_MODEL_ID = process.env.COHERE_FEEDBACK_MODEL_ID;
+const FEEDBACK_SCORE_THRESHOLD = 0.8;
+const DISCORD_API_KEY = process.env.DISCORD_API_KEY
+const DISCORD_GENERAL_CHANNEL_ID = process.env.DISCORD_GENERAL_CHANNEL_ID;
+const DISCORD_FEEDBACK_CHANNEL_ID = process.env.DISCORD_FEEDBACK_CHANNEL_ID;
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ 
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	] 
+});
+
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
@@ -12,5 +29,12 @@ client.once(Events.ClientReady, c => {
 });
 
 // Log in to Discord with your client's token
-client.login(token);
+client.login(DISCORD_API_KEY);
 
+
+client.on('messageCreate', (message) => {
+	console.log(message.content);
+	console.log(message.createdAt.toDateString);
+	console.log(message.author.tag);
+
+});
